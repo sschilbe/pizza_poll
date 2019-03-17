57d98bc7c52db5976f33447090c78efea4fbc50b
Seth Schilbe <seth.schilbe@gmail.com>
2019-03-17 13:24:32 -0300

    Fix stupid error

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request, send_from_directory
 from pizzapi import *
-import os
-import json
 
 # Initialize Flask Application
 app = Flask(__name__)
 
-@app.route('')
+@app.route('/')
 def hello_world():
     return "Hello World!"
